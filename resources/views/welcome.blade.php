4b4628adc2dd7645276494a51ca3dd40c2249bbe
Awais-Koder <awaistayyab27@gmail.com>
2025-04-03 10:58:32 +0500

    Auto commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <title>Orion</title>
 
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @else
-        @endif
-    </head>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <!-- fialment / Scripts -->
+    @filamentStyles
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="">
+        <div class="flex flex-col items-center justify-center min-h-screen py-6 sm:py-12 lg:py-24">
+            <div class="text-center">
+                <h1 class="text-4xl font-bold text-gray-900">Welcome to Orion</h1>
+                <p class="mt-4 text-lg text-gray-600">Your one-stop solution for all your needs.</p>
+            </div>
+        </div>
+    </div>
+    @filamentScripts
+</body>
 
 </html>
